1bcc67d6ffc38c4c4c432cdd6e61ee63912b84ec
josimot <josimota2212@outlook.com>
2022-06-20 20:33:48 -0300

    loops

---
+++ ex1.js
@@ -0,0 +1,9 @@
+ vamos peliculas = ["star wars",, "totoro", "rocky", "pulp fiction",, "la vida es bella"]
+ 
+  conversor de funçãoAMayusculas(peliculas ){
+      para (deixe x=0; x<peliculas. comprimento; x++){
+          peliculas[x] = peliculas[x]. toUpperCase()
+      }
+      peliculas de retorno;
+  }
+   console. log(conversorAMayusculas(peliculas))
